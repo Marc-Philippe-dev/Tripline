edefc67ca3c30a07b745566bcc3fc80eb0028a4b
Cyriac Thibaudeau <cyriacthibaudeau92@gmail.com>
2025-02-12 18:08:28 +0100

    Register v0

---
+++ src/app/register/page.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import { Input } from "@/components/ui/input";
+
+export default function Register() {
+  return (
+    <section>
+      <div className="container m-auto grid grid-cols-1 md:grid-cols-2 items-center">
+        <div className="container m-auto grid items-center">
+          <h2>S'inscrire</h2>
+          <form action="" className="mt-6 space-y-4 w-96">
+            <Input type="email" placeholder="E-mail" />
+            <Input type="password" placeholder="Mot de passe" />
+            <Input type="password" placeholder="Confirmer le mot de passe" />
+          </form>
+        </div>
+        <div>
+          <Image
+            src="/hero-image.svg"
+            alt="checkmark"
+            width={1000}
+            height={1000}
+          ></Image>
+        </div>
+      </div>
+    </section>
+  );
+}

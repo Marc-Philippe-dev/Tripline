cbf9862e4373570dab7c5efc1d5eca0ac01b9483
GNANCADJA Marc Philippe <marcgnancadja4@gmail.com>
2025-03-11 20:34:48 +0100

    Trip creation implemented

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,21 @@
-import ProtectedRoutes from "@/utils/protectedRoutes";
+"use client";
 import React from "react";
+import ProtectedRoutes from "@/utils/protectedRoutes";
+ 
+import CreateTrip from "./(components)/createTrip";
 
 export default function Dashboard() {
+
   return (
     <ProtectedRoutes>
-      <div>Dashboard</div>
+      <div>
+        <h1 className="text-center capitalize text-gray-600">
+          Bienvenu dans votre espace
+        </h1>
+        <div className="flex justify-center mt-10">
+         <CreateTrip />
+        </div>
+      </div>
     </ProtectedRoutes>
   );
 }

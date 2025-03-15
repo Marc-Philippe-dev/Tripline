39e1f892de0da96ccf9384051ff99f4de00775ea
yassine <yassinemeudon@gmail.com>
2025-03-15 09:00:00 +0100

    gestion des erreurs

--- src/utils/uploadImage.js
+++ src/utils/uploadImage.js
@@ -1,10 +1,13 @@
 import axiosInstance from "./axiosInstance";
 
+
+
 const uploadImage = async (imageFile) => {
   const formData = new FormData();
   // Append image file to form data
   formData.append('image', imageFile); 
 
+  
   try {
     const response = await axiosInstance.post('/image-upload', formData, {
       headers: {
@@ -16,6 +19,8 @@ const uploadImage = async (imageFile) => {
     console.error('Error uploading the image:', error); 
     throw error; // Rethrow error for handling
   }
+
+  
 };
 
 export default uploadImage;
